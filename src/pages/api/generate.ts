24ae940223067515bf8d56ee8d16c064777bdaaf
tareefroustom <58273854+tareefroustom@users.noreply.github.com>
2023-05-21 03:28:07 +0200

    Update generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,18 +1,18 @@
-import type { APIRoute } from 'astro'
-import { generatePayload, parseOpenAIStream } from '@/utils/openAI'
-import { verifySignature } from '@/utils/auth'
+import type { APIRoute } from 'astro';
+import { generatePayload, parseOpenAIStream } from '@/utils/openAI';
+import { verifySignature } from '@/utils/auth';
 // #vercel-disable-blocks
-import { fetch, ProxyAgent } from 'undici'
+import { fetch, ProxyAgent } from 'undici';
 // #vercel-end
 
-const apiKey = import.meta.env.OPENAI_API_KEY
-const httpsProxy = import.meta.env.HTTPS_PROXY
-const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/,'')
-const sitePassword = import.meta.env.SITE_PASSWORD
+const apiKey = import.meta.env.OPENAI_API_KEY;
+const httpsProxy = import.meta.env.HTTPS_PROXY;
+const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/, '');
+const sitePassword = import.meta.env.SITE_PASSWORD;
 
-async function concatenateMessages(json) {
+async function concatenateMessages(json: string) {
   try {
-    const obj = await JSON.parse(json);
+    const obj = JSON.parse(json);
     const messages = obj.messages;
     let concatenatedContents = "";
 
@@ -28,22 +28,9 @@ async function concatenateMessages(json) {
 }
 
 export const post: APIRoute = async (context) => {
-  
-  //const body = await context.request.json();
-  //const question = await concatenateMessages(JSON.stringify(body));
-  
-  
-  //const referer = await context.request.headers.get('referer');
-  //const searchParams = new URLSearchParams(referer.split('?')[1]);
-  //console.log(searchParams);
-  //const documentUrl = searchParams.get('Document_URL');
-  //const userid = searchParams.get('id');
-  //console.log(userid);
-  //const params = new URLSearchParams(context.request.headers.referer.split('?')[0]);
-  //const documentUrl = params.get('Document_URL');
-  
-  
-  //console.log(documentUrl);
+  const body = await context.request.json();
+  const question = await concatenateMessages(JSON.stringify(body));
+
   const response = await fetch('https://nnq4xy5uj3.execute-api.eu-west-1.amazonaws.com/dev/call', {
     method: 'POST',
     headers: {
@@ -51,12 +38,8 @@ export const post: APIRoute = async (context) => {
     },
     body: JSON.stringify({
       bot_id: "6e6cf576-69e1-410b-9036-d870922f556a",
-      //sheet_name: 'gpt panda',
-      //selected_sheet: ['Tester'],
       operation: 'ask-bot',
-      question: question,
-      //document_url: documentUrl,
-      //...body // include any additional data from the original request body
+      question: question
     })
   });
 
@@ -64,10 +47,9 @@ export const post: APIRoute = async (context) => {
     throw new Error(`Request failed with status ${response.status}`);
   }
 
-  const responseData = await response.json(); 
-  console.log(responseData)
-  //const results = responseData.results.split('\n\n');
-  //const text = results.join('\n');
+  const responseData = await response.json();
+  console.log(responseData);
+
   return new Response(responseData.results, {
     headers: {
       'Content-Type': 'text/plain'
