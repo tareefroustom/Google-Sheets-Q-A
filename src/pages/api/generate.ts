34f583de427dd6da16bedb6ca82f05923e7d141f
tareefroustom <58273854+tareefroustom@users.noreply.github.com>
2023-05-21 03:33:20 +0200

    Update generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -28,31 +28,5 @@ async function concatenateMessages(json: string) {
 }
 
 export const post: APIRoute = async (context) => {
-  const body = await context.request.json();
-  const question = await concatenateMessages(JSON.stringify(body));
-
-  const response = await fetch('https://nnq4xy5uj3.execute-api.eu-west-1.amazonaws.com/dev/call', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      bot_id: "6e6cf576-69e1-410b-9036-d870922f556a",
-      operation: 'ask-bot',
-      question: question
-    })
-  });
-
-  if (!response.ok) {
-    throw new Error(`Request failed with status ${response.status}`);
-  }
-
-  const responseData = await response.json();
-  console.log(responseData);
-
-  return new Response(responseData.results, {
-    headers: {
-      'Content-Type': 'text/plain'
-    }
-  });
+return "Tester tester"
 };
